a670fc75300dc2eeb2fc382fdd3bf4b87a858f5d
LJY <dayanglee2015@163.com>
2018-01-11 14:01:39 +0800

    update 7.py

--- 7.py
+++ 7.py
@@ -116,4 +116,57 @@ print(power1(5, 3))  # 125
 # 二是如何设置默认参数
 # 当函数有多个参数时，把变化大的参数放前面，变化小的参数放后面。变化小的参数就可以作为默认参数
 # 使用默认参数有什么好处？最大的好处是能降低调用函数的难度。
+# 默认参数必须指向不变对象！！！
+
+
+def enroll(name, gender, age=6, city='Beijing'):
+    print('name:', name)
+    print('gender:', gender)
+    print('age:', age)
+    print('city:', city)
+
+
+print(enroll('Dayang', 'F'))
+print(enroll('JY', 'M', 24, 'Xi\'an'))
+
+
+# 可变参数
+# 在Python函数中，还可以定义可变参数。顾名思义，可变参数就是传入的参数个数是可变的，可以是1个、2个到任意个，还可以是0个。
+# 要定义出这个函数，我们必须确定输入的参数。由于参数个数不确定，我们首先想到可以把a，b，c……作为一个list或tuple传进来，这样，函数可以定义如下
+def calc(numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+
+
+print(calc([1, 2, 3]))  # 14
+
+
+# 改为可变参数，定义为下
+def calc1(*numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+
+
+print(calc1(1, 2, 3))  # 14
+nums = [1, 2, 3]
+# *nums表示把nums这个list的所有元素作为可变参数传进去。这种写法相当有用，而且很常见。
+print(calc1(*nums))  # 14
+
+# 关键字参数
+# 可变参数允许你传入0个或任意个参数，这些可变参数在函数调用时自动组装为一个tuple。而关键字参数允许你传入0个或任意个含参数名的参数，这些关键字参数在函数内部自动组装为一个dict
+# 函数person除了必选参数name和age外，还接受关键字参数kw。在调用该函数时，可以只传入必选参数, 也可以传入任意个数的关键字参数
+# 关键字参数有什么用？它可以扩展函数的功能。比如，在person函数里，我们保证能接收到name和age这两个参数，但是，如果调用者愿意提供更多的参数，我们也能收到。试想你正在做一个用户注册的功能，除了用户名和年龄是必填项外，其他都是可选项，利用关键字参数来定义这个函数就能满足注册的需求。
+
+def person(name, age, **kw):
+    print('name:', name, 'age:', age, 'other:', kw)
+
+
+person('Dayang', 24)  # name: Dayang age: 24 other: {}
+person('JY', 23, city='Beijing', Job="nngineer")  # name: JY age: 23 other: {'Job': 'nngineer', 'city': 'Beijing'}
+extra = {'city': 'Beijing', 'job': 'Engineer'}
+person('Jack', 24, **extra)  # name: Jack age: 24 other: {'job': 'Engineer', 'city': 'Beijing'}
 
